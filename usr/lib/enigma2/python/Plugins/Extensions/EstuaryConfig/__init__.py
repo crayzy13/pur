4f539fcdd84db8f237ee4efdc4bb4aaec2e7678a
crayzy13 <manalomri558@gmail.com>
2017-10-09 16:47:09 +0100

    first commit

---
+++ usr/lib/enigma2/python/Plugins/Extensions/EstuaryConfig/__init__.py
@@ -0,0 +1,18 @@
+from Components.Language import language
+from os import environ as os_environ
+import gettext
+
+myPlugin = "EstuaryConfig"
+
+def localeInit():
+	os_environ["LANGUAGE"] = language.getLanguage()[:2]
+	gettext.bindtextdomain(myPlugin, ("/usr/lib/enigma2/python/Plugins/Extensions/"+myPlugin+"/locale"))
+
+def _(txt):
+	t = gettext.dgettext(myPlugin, txt)
+	if t == txt:
+		t = gettext.gettext(txt)
+	return t
+
+localeInit()
+language.addCallback(localeInit)
